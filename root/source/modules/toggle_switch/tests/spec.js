2c54b8f0486ae4480d98cc985e1b359ffb0d24ab
ironikart <abarnes@squiz.com.au>
2013-05-29 09:21:16 +1000

    Added some basic unit tests for the toggle switcher

---
+++ root/source/modules/toggle_switch/tests/spec.js
@@ -0,0 +1,19 @@
+test('The plugin has created the toggle switch ui element', function() {
+  expect(1);
+  equal($('.toggle-switch-ui').length, 1, 'The toggle switcher has created content.');
+});
+
+test('The select function works as expected', function() {
+  expect(3);
+  var toggle = $.data($('.toggle-switch').get(0),'toggle_switch');
+  equal(typeof(toggle.select), 'function', 'The selection property is a function');
+
+  toggle.select(0);
+
+  equal($('.toggle-switch-active').length, 0, 'The toggle has de-activated');
+
+  toggle.select(1);
+
+  equal($('.toggle-switch-active').length, 1, 'The toggle has activated');
+});
+
