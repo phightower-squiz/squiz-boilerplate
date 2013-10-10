01e791c7506ab94ecb91d8fea4f7ebee066adb3c
Anthony Barnes <abarnes@squiz.net>
2013-10-10 15:55:12 +1100

    Fixed the chart into a proper working example

--- root/source/modules/chart/js/global.js
+++ root/source/modules/chart/js/global.js
@@ -1,26 +1,37 @@
 /*global google:false*/
-(function($) {
-    var options = {
-        chartClass: 'chart'
-    };
+// Globally scoped chart loader
+function loadCharts() {
+    google.load('visualization', '1', {'callback':'', 'packages':['corechart']});
+    google.setOnLoadCallback(drawChart);
+}//end loadCharts()
 
-    // Load charts using the google script loader
-    function loadCharts() {
-        // Callback is required here to force google charts to use append rather
-        // than document.write which will destroy the page.
-        // http://stackoverflow.com/questions/9519673/why-does-google-load-cause-my-page-to-go-blank
-        google.load('visualization', '1', {'callback':'', 'packages':['corechart']});
-        google.setOnLoadCallback(drawChart);
-    }//end loadCharts()
+var drawChart = (function($) {
+    function initLoader() {
+        var script = document.createElement("script");
+        script.src = "https://www.google.com/jsapi?callback=loadCharts";
+        script.type = "text/javascript";
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }//end initLoader
 
-    // Draw the chart
-    function drawChart() {
-        var $charts = $('.' + options.chartClass);
+    // Script loader when google charts is not available
+    $(document).ready(function() {
+
+        // Only proceed if we have google chart element on the
+        // page indicating we need this script loader.
+        if (!$('.chart').length) {
+            return;
+        }//end if
+
+        initLoader();
+    });
+
+    return function() {
+        var $charts = $('.chart');
         $charts.each(function(){
             // Create the data table.
             var data = new google.visualization.DataTable();
-            data.addColumn('number', 'Asset');
-            data.addColumn('date', 'Date');
+            data.addColumn('string', 'Type');
+            data.addColumn('number', 'Number');
             data.addRows([
               ['Mushrooms', 3],
               ['Onions', 1],
@@ -32,27 +43,12 @@
             // Set chart options
             var options = {
                 title:'How Much Pizza I Ate Last Night',
-                width:400,
-                height:300,
                 colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6']
             };
 
             // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.LineChart($(this).find('.chart-output').get(0));
+            var chart = new google.visualization.PieChart($(this).find('.chart-output').get(0));
             chart.draw(data, options);
         });
-    }//end drawChart()
-
-    // Script loader when google charts is not available
-    $(document).ready(function() {
-
-        // Only proceed if we have google chart element on the
-        // page indicating we need this script loader.
-        if (!$('.' + options.chartClass).length) {
-            return;
-        }//end if
-
-        loadCharts();
-
-    });
+    };
 }(jQuery));
