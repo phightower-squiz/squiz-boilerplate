76c27eafe12e34c23962b7a3d65da7be8cf9f54d
Anthony Barnes <abarnes@squiz.net>
2013-10-18 21:09:47 +1100

    Replaced the google chart example with something that reflects better progressive enhancement. Example plugin draws the values from the dom to populate the google dataTable

--- root/source/modules/chart/js/global.js
+++ root/source/modules/chart/js/global.js
@@ -1,54 +1,19 @@
-/*global google:false*/
-// Globally scoped chart loader
-function loadCharts() {
-    google.load('visualization', '1', {'callback':'', 'packages':['corechart']});
-    google.setOnLoadCallback(drawChart);
-}//end loadCharts()
+(function($, undefined) {
+    'use strict';
+    $(document).ready(function(){
+        $('.chart').google_chart({
 
-var drawChart = (function($) {
-    function initLoader() {
-        var script = document.createElement("script");
-        script.src = "https://www.google.com/jsapi?callback=loadCharts";
-        script.type = "text/javascript";
-        document.getElementsByTagName("head")[0].appendChild(script);
-    }//end initLoader
+            // Chart options map directly to the google chart visualisation options
+            // See: https://developers.google.com/chart/interactive/docs/customizing_charts
+            chart: {
+                //title: 'Chart Title',
+                is3D: true
+            },
 
-    // Script loader when google charts is not available
-    $(document).ready(function() {
-
-        // Only proceed if we have google chart element on the
-        // page indicating we need this script loader.
-        if (!$('.chart').length) {
-            return;
-        }//end if
-
-        initLoader();
-    });
-
-    return function() {
-        var $charts = $('.chart');
-        $charts.each(function(){
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Type');
-            data.addColumn('number', 'Number');
-            data.addRows([
-              ['Mushrooms', 3],
-              ['Onions', 1],
-              ['Olives', 1],
-              ['Zucchini', 1],
-              ['Pepperoni', 2]
-            ]);
-
-            // Set chart options
-            var options = {
-                title:'How Much Pizza I Ate Last Night',
-                colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6']
-            };
-
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.PieChart($(this).find('.chart-output').get(0));
-            chart.draw(data, options);
+            // When the chart has finished loading this callback will be triggered
+            callbacks: {
+                loaded: function() {}
+            }
         });
-    };
+    });
 }(jQuery));
---
+++ root/source/modules/chart/js/plugin.js
@@ -0,0 +1,155 @@
+/*global jquery:false*/
+/**
+ * Boilerplate wrapper for invoking google charts
+ */
+(function($, undefined){
+    'use strict';
+
+    var callbackName = '__loadCharts';
+
+    var isLoadCalled = false;
+
+    // Default configuration
+    var defaults = {
+        classes: {
+            ignore: 'chart-ignore',
+            output: 'chart-output',
+            loaded: 'chart-loaded',
+            data:   'chart-data'
+        },
+        loaded: function() {}
+    };
+
+    var instantiatedCharts = [];
+
+    // Be a good citizen and allow restoration of the old chart function if
+    // present.
+    var oldLoader = window[callbackName];
+    var loadCharts = window[callbackName] = function() {
+        if (window.google) {
+            window.google.load('visualization', '1', {'callback':'', 'packages':['corechart']});
+            window.google.setOnLoadCallback(function() {
+                for (var i = 0, l = instantiatedCharts.length; i<l; i++) {
+                    instantiatedCharts[i].init();
+                }//end for
+            });
+        }//end if
+    };//end __loadCharts
+
+    ///////////////////////
+    // Private functions //
+    ///////////////////////
+
+    // Load the Google JS API, but only load it once
+    function loadGoogleJSAPI() {
+        if (!window.google && !isLoadCalled) {
+            var script = document.createElement("script");
+            script.src = "https://www.google.com/jsapi?callback=" + callbackName;
+            script.type = "text/javascript";
+            document.getElementsByTagName("head")[0].appendChild(script);
+            isLoadCalled = true;
+        }//end if
+    }//end loadGoogleJSAPI()
+
+    // Constructor
+    var GoogleChart = function(elem, options) {
+        this.settings = $.extend({}, defaults, options, true);
+        this.elem     = elem;
+        this.$elem    = $(elem);
+
+        // Add the chart to an internal list ready to init after load
+        instantiatedCharts.push(this);
+        loadGoogleJSAPI();
+    };
+
+    //////////////////////
+    // Public functions //
+    //////////////////////
+
+    GoogleChart.prototype = {
+
+        /**
+         * Initialise the chart
+         */
+        init: function() {
+            this.loadData();
+            this.draw();
+            this.$elem.addClass(this.settings.classes.loaded);
+            this.$elem.trigger('google-chart:loaded');
+        },
+
+        /**
+         * Load the chart data from the DOM
+         */
+        loadData: function() {
+            var self = this;
+            var $data = $('.' + self.settings.classes.data, self.$elem);
+            var columns = [];
+
+            // Create a google data table.
+            self.dataTable = new window.google.visualization.DataTable();
+
+            // Columns are defined in the header rows
+            $('thead th', self.$elem).each(function() {
+                if (!$(this).hasClass(self.settings.classes.ignore)) {
+                    var type = $(this).data('type');
+                    columns.push(type);
+                    self.dataTable.addColumn(type, $(this).text());
+                }//end if
+            });
+
+            $('tbody tr', self.$elem).each(function() {
+                if (!$(this).hasClass(self.settings.classes.ignore)) {
+                    var $cells = $('td', this);
+                    var row = $.map($cells, function(cell, i) {
+                        var cellValue = $(cell).text();
+                        switch(columns[i]) {
+                            case 'number':
+                                cellValue = parseFloat(cellValue);
+                            break;
+                            case 'date':
+                                cellValue = new Date(cellValue);
+                            break;
+                        }//end switch
+                        return cellValue;
+                    });
+                    self.dataTable.addRow(row);
+                }//end if
+            });
+        },//end loadData()
+
+        /**
+         * Draw the chart
+         */
+        draw: function() {
+            var type = this.$elem.data('chart-type') || 'PieChart';
+            var $output = $('.' + this.settings.classes.output, this.$elem);
+            console.log('draw', $output);
+            var options = this.settings.chart;
+            if (typeof(this.dataTable) !== 'undefined' &&
+                window.google.visualization.hasOwnProperty(type) &&
+                $output.length) {
+                var chart = new window.google.visualization[type]($output.get(0));
+                chart.draw(this.dataTable, options);
+            }//end if
+            this.settings.loaded.call(this);
+        },//end draw()
+
+        /**
+         * Restore the global load function
+         */
+        loaderNoConflict: function() {
+            window[callbackName] = oldLoader;
+            return loadCharts;
+        }//end loaderNoConflict()
+    };
+
+    // jQuery plugin
+    $.fn.google_chart = function(options) {
+        return this.each(function () {
+            if (!$.data(this, 'google_chart')) {
+                $.data(this, 'google_chart', new GoogleChart( this, options ));
+            }
+        });
+    };
+}(jQuery));
