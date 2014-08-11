8dae722d3d3883cdf83df6aa96d7d42c0c481284
Anthony Barnes <abarnes@squiz.net>
2014-08-11 11:16:59 +1000

    Added version detection and commenting

--- app/templates/lib/defer.js
+++ app/templates/lib/defer.js
@@ -20,6 +20,8 @@ var defer = {
     _tasks: {}
 };
 
+// Run the task
+// Load the NPM task only if not ignored and a package name is supplied
 defer.runTask = function(task, pkgName) {
     defer.grunt.verbose.writeln('Run task', task.yellow);
     if (pkgName && !defer.ignore.test(pkgName)) {
@@ -28,10 +30,22 @@ defer.runTask = function(task, pkgName) {
     defer.grunt.task.run(task);
 };
 
+// Parse the name and version out of a package name
+defer.parsePackageName = function(name) {
+    if (name.indexOf('@') === -1) {
+        return { name: name, version: '' };
+    } else {
+        var parts = name.split('@');
+        return { name: parts[0], version: parts[1] };
+    }
+};
+
+// Use the file system to check if the module is already installed
 defer.isInstalled = function(name) {
     return defer.grunt.file.exists(path.resolve(defer.pkgRoot, name));
 };
 
+// Install the module by spawning `npm install [pkgName] --save`
 defer.installModule = function(name, args, cb) {
     var cmd = 'npm install ' + name + ' ' + args.join(' ');
     defer.grunt.log.writeln('Installing deferred NPM: ' + name.yellow);
@@ -44,16 +58,22 @@ defer.installModule = function(name, args, cb) {
     });
 };
 
-defer.fetchTaskName = function(npmName) {
+// Fetch a task name parsed from the package name
+// e.g. grunt-contrib-jshint becomes jshint
+defer.fetchTaskName = function(pkgName) {
+    var pkg = defer.parsePackageName(pkgName);
     return _.reduce(PREFIXES, function(memo, prefix) {
         var reg = new RegExp('^' + prefix.replace('-', '\\-'));
         return memo.replace(reg, '');
-    }, npmName);
+    }, pkg.name);
 };
 
+// Proxy a list of packages associated with a task and arrange
+// for them to be installed asynchronously as required
 defer.proxy = function(packages, task) {
     var allInstalled = _.every(packages, function(pkgName) {
-        return defer.isInstalled(pkgName);
+        var pkg = defer.parsePackageName(pkgName);
+        return defer.isInstalled(pkg.name);
     });
 
     // No further action required
@@ -69,13 +89,14 @@ defer.proxy = function(packages, task) {
         defer.grunt.task.renameTask(task, '_'+task+'_');
 
         async.forEachSeries(packages, function (pkgName, next) {
-            if (!defer.isInstalled(pkgName)) {
+            var pkg = defer.parsePackageName(pkgName);
+            if (!defer.isInstalled(pkg.name)) {
                 defer.installModule(pkgName, ['--save'], function() {
-                    defer.runTask(runName, pkgName);
+                    defer.runTask(runName, pkg.name);
                     next();
                 });
             } else {
-                defer.runTask(runName, pkgName);
+                defer.runTask(runName, pkg.name);
                 next();
             }//end if
         }, function() {
@@ -84,6 +105,7 @@ defer.proxy = function(packages, task) {
     });
 };
 
+// Each task can have multiple package dependencies associated
 defer.pushTask = function(taskName, pkgName) {
     var tasks = defer._tasks;
     if (tasks.hasOwnProperty(taskName)) {
@@ -95,6 +117,15 @@ defer.pushTask = function(taskName, pkgName) {
     defer._tasks = tasks;
 };
 
+// Takes an array of package names
+// If a hash is supplied package names are associated with tasks manually,
+// otherwise the assignment is automated based on the list of PREFIXES
+// e.g.
+// [
+//  'grunt-contrib-jshint',
+//  'grunt-contrib-concat@0.5.0',
+//  { 'grunt-contrib-connect': ['serve'] }
+// ]
 module.exports = function deferLoad(grunt, map) {
     if (!defer.grunt) {
         defer.grunt = grunt;
