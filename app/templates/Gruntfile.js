fa6644a01ad4847a9d13399d52ca7d118571b2e0
Anthony Barnes <abarnes@squiz.net>
2014-09-09 11:36:59 +1000

    Fixed output for banners

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function (grunt) {
+    var path = require('path');
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
@@ -143,11 +144,15 @@ module.exports = function (grunt) {
                     ]
                 },
                 banner:  function(file) {
-
-                    // var basename = path.basename(file);
-                    // if (basename.indexOf('variables') === -1) {
-                    //     return '/*-- ' + path.basename(file) + ' --*/\n';
-                    // }
+                    var basename = path.basename(file);
+                    var isModule = (file.indexOf(tasks.config.source + '/modules/') !== -1)
+                        || (file.indexOf(tasks.config.source + '/squiz-module-') !== -1);
+                    if (isModule) {
+                        var moduleName = file.split(/\//g)[2];
+                        if (/\.(s)?css/.test(basename) && basename.indexOf('variables') === -1) {
+                             return '/*-- module:' + moduleName + ' --*/\n';
+                        }
+                    }
                     return;
                 }
             },
